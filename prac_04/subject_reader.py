38fe0c295cb392e517c07d83c18f16d2a5e2ce88
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-07 14:25:32 +0800

    Refactor a format print function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,30 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    classes = load_data(FILENAME)
+    print_info(classes)
 
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    pairs = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        pairs.append(parts)  # append list to pairs list
     input_file.close()
+    return pairs
+
+
+def print_info(classes: list):
+    course_width = max(len(part[0]) for part in classes)
+    name_width = max(len(part[1]) for part in classes)
+    number_width = max(len(str(part[2])) for part in classes)
+
+    for course, name, number in classes:
+        print(f"{course:{course_width}} is taught by {name:{name_width}} and has {number:{number_width}} students.")
 
 
 main()
