ad1f87b50aab128cee741eec3ddc7966d9336fa7
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-07 16:15:50 +0800

    Add function check username

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,9 +1,13 @@
-
 NUMBER_LENGTH = 5
+USERNAME = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+            'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
 
 def main():
     numbers = get_valid_number(NUMBER_LENGTH)
     print_info(numbers)
+    user_name = input("please enter your username")
+    check_username(USERNAME, user_name)
 
 
 def get_valid_number(number_length):
@@ -19,11 +23,19 @@ def get_valid_number(number_length):
                 print("Invalid number")
     return numbers
 
+
 def print_info(numbers):
     print(f"The first number is {numbers[0]}")
     print(f"The last number is {numbers[-1]}")
     print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
-    print(f"The average of number is {sum(numbers)/len(numbers)}")
+    print(f"The average of number is {sum(numbers) / len(numbers)}")
+
+
+def check_username(USERNAME , user_name):
+    if user_name not in USERNAME:
+        print("Access denied")
+    else:
+        print("Access granted")
 
 main()
