8c34d2fd478acad6380d68245182ab6d64b2c0db
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-13 23:01:42 +0800

    Add 10 colour names in COLOR_TO_CODE dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,6 @@
+COLOR_TO_CODE = {"Azure1": "#f0ffff", "Azure2": "#e0eeee", "Azure3":"#c1cdcd","Azure4":"#838b8b","Aquamarine1":"#7fffd4","Aquamarine2":"#76eec6","Aquamarine4":"#458b74","Asparagus":"#87a96b","Baby Blue":"#89cff0","Baby Pink":"#f4c2c2"}
+
+
+def main():
+
+main()
