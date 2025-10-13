4cb23c0f358676cbbdd0806142e9ac206b72f859
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-14 00:48:20 +0800

    Define a dictionary and put words as key and count as value into the dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,6 +1,9 @@
 def main():
     user_sentence = input("Enter any sentence you like:")
     words = user_sentence.strip().replace(".","").split(" ")
+    word_to_count = {}
+    for word in words:
+        word_to_count[word] = word_to_count.get(word, 0) + 1
 
 
 
