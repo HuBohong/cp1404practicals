055fc8fe8f6025a3d506e69b752be7fdbc5d8848
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-14 01:05:56 +0800

    print dictionary in format

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,5 +8,7 @@ def main():
             word_to_count[word] = word_to_count.get(word, 0) + 1
         user_sentence = input("Enter any sentence you like:")
 
-
+    print("Text: this is a collection of words of nice words this is a fun thing it is")
+    for word, count in sorted(word_to_count.items()):
+        print(f"{word}:{count}")
 main()
