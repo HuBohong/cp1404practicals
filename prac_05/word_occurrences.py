ed5573643f6b3858bf5718c2fa08e8f99182a21c
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-14 01:11:13 +0800

    Count format word width and count width and adjust the print format

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -7,8 +7,12 @@ def main():
         for word in words:
             word_to_count[word] = word_to_count.get(word, 0) + 1
         user_sentence = input("Enter any sentence you like:")
+    word_width = max(len(word) for word in word_to_count.keys())
+    count_width = max(len(count) for count in word_to_count.values())
 
     print("Text: this is a collection of words of nice words this is a fun thing it is")
     for word, count in sorted(word_to_count.items()):
-        print(f"{word}:{count}")
+        print(f"{word:{word_width}}:{count: {count_width}}")
+
+
 main()
