6edd20eb74fd988ca6707d6dbc043f9e542f8da4
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-14 00:51:34 +0800

    Refactor code by repeat ask user to input sentence and store words in dictionary

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,10 +1,12 @@
 def main():
-    user_sentence = input("Enter any sentence you like:")
-    words = user_sentence.strip().replace(".","").split(" ")
     word_to_count = {}
-    for word in words:
-        word_to_count[word] = word_to_count.get(word, 0) + 1
+    user_sentence = input("Enter any sentence you like:")
+    while user_sentence != "":
 
+        words = user_sentence.strip().replace(".", "").split(" ")
+        for word in words:
+            word_to_count[word] = word_to_count.get(word, 0) + 1
+        user_sentence = input("Enter any sentence you like:")
 
 
 main()
