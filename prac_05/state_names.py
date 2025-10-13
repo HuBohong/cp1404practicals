aecbfad2e21dfe13429261764b90f42ab91765e3
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-13 22:25:39 +0800

    calculate width of shortcut and province then print in format

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,6 +9,9 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
 
+shortcut_width = max(len(province_shortcut) for province_shortcut in CODE_TO_NAME.keys())
+province_width = max(len(province_name) for province_name in CODE_TO_NAME.values())
+
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
@@ -17,4 +20,5 @@ while state_code != "":
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
 
-
+for province_shortcut, province in CODE_TO_NAME.items():
+    print(f"{province_shortcut:{shortcut_width}} is {province:{province_width}}")
