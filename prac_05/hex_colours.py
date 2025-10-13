5dc5cabba8e53c75f411007cc592cd67b293e079
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-13 23:21:54 +0800

    Add user input validation method

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,6 +1,19 @@
-COLOR_TO_CODE = {"Azure1": "#f0ffff", "Azure2": "#e0eeee", "Azure3":"#c1cdcd","Azure4":"#838b8b","Aquamarine1":"#7fffd4","Aquamarine2":"#76eec6","Aquamarine4":"#458b74","Asparagus":"#87a96b","Baby Blue":"#89cff0","Baby Pink":"#f4c2c2"}
+COLOR_TO_CODE = {"Azure1": "#f0ffff", "Azure2": "#e0eeee", "Azure3": "#c1cdcd", "Azure4": "#838b8b",
+                 "Aquamarine1": "#7fffd4", "Aquamarine2": "#76eec6", "Aquamarine4": "#458b74", "Asparagus": "#87a96b",
+                 "Baby Blue": "#89cff0", "Baby Pink": "#f4c2c2"}
 
 
 def main():
+    user_color = get_valid_color()
+
+
+def get_valid_color():
+    user_color = input("Enter color code: ").title()
+    while user_color not in COLOR_TO_CODE:
+        print("Invalid color name")
+        user_color = input("Enter color code: ").title()
+
+    return user_color
+
 
 main()
