64d1dabc1f1951a809ce75575cb3b1f9df9338e2
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-14 13:48:52 +0800

    Reformat email address to ask user's name

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -17,6 +17,8 @@ def get_user_info(email_to_name):
     while EMAIL_SIGN not in user_email:
         user_email = input("Please enter a correct email address")
 
+    while user_email != "":
+        user_name = user_email.strip().replace(".", " ").split("@")[0].title()
 
 
 
