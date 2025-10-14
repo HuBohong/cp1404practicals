e447ebf10c955c558a8affba68d7d499ad9ab395
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-14 16:07:41 +0800

    Calculate champion count and sorted campion country

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -11,13 +11,16 @@ FILENAME = "wimbledon.csv"
 
 
 def main():
-    with open(FILENAME,"r", encoding="utf-8-sig") as in_file:
+    champion_to_count = {}
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         reader = csv.reader(in_file)
         next(reader)
-        information_champions = [[information[1],information[2]] for information in list(reader)]
-        print(information_champions)
+        information_champions = [[information[1], information[2]] for information in list(reader)]
+        for pair in information_champions:
+            champion_to_count[pair[1]] = champion_to_count.get(pair[1], 0) + 1
 
-
+        champion_country = set(pair[0] for pair in information_champions)
+        print(sorted(champion_country))
 
 
 main()
