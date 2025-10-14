78892dc89259d5b8bd38794235842fae2e6a8a53
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-14 14:08:11 +0800

    Refactor code by function only do one thing to separate get email and get name function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -9,24 +9,28 @@ EMAIL_SIGN = "@"
 
 def main():
     email_to_name = {}
-    get_user_info(email_to_name)
-
-
-def get_user_info(email_to_name):
     user_email = input("Enter your email address: ")
-    while EMAIL_SIGN not in user_email:
-        user_email = input("Please enter a correct email address")
-
     while user_email != "":
-        user_name = user_email.strip().replace(".", " ").split("@")[0].title()
-        is_username = input(f"Is your name {user_name}? (Y/n)")
-        if is_username.upper() == "Y":
-            email_to_name[user_email] = user_name
-            user_email = input("Enter your email address: ")
-        else:
-            user_name = input("Please enter your name")
-            email_to_name[user_email] = user_name
-            user_email = input("Enter your email address: ")
+        user_email = get_validate_email(user_email)
+        user_name = get_user_name(user_email)
+        email_to_name[user_email] = user_name
 
+        user_email = input("Enter your email address: ")
+
+
+def get_validate_email(user_email):
+    while EMAIL_SIGN not in user_email:
+        user_email = input("Please enter a correct email address:")
+    return user_email
+
+
+def get_user_name(user_email):
+    user_name = user_email.strip().replace(".", " ").split("@")[0].title()
+    is_username = input(f"Is your name {user_name}? (Y/n)")
+    if is_username.upper() == "Y":
+        return user_name
+    else:
+        user_name = input("Please enter your name").title()
+        return user_name
 
 main()
