cb73fad4642e47c98a33e45544ce29a05bdc2ee2
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-14 16:24:58 +0800

    Refact the code from waterfull to 4 functions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -12,15 +12,36 @@ FILENAME = "wimbledon.csv"
 
 def main():
     champion_to_count = {}
+    information_champions = get_champion_information()
+    count_campion(champion_to_count, information_champions)
+
+    champion_country = get_distinct_champion_country(information_champions)
+    print_format_information(champion_country, champion_to_count)
+
+
+def print_format_information(champion_country, champion_to_count):
+    for name, count in champion_to_count.items():
+        print(f"{name}: {count}")
+    print(f"These 12 countries have won Wimbledon:")
+    print(", ".join(sorted(champion_country)))
+
+
+def get_distinct_champion_country(information_champions):
+    champion_country = set(pair[0] for pair in information_champions)
+    return champion_country
+
+
+def get_champion_information():
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
         reader = csv.reader(in_file)
         next(reader)
         information_champions = [[information[1], information[2]] for information in list(reader)]
-        for pair in information_champions:
-            champion_to_count[pair[1]] = champion_to_count.get(pair[1], 0) + 1
+        return information_champions
 
-        champion_country = set(pair[0] for pair in information_champions)
-        print(sorted(champion_country))
+
+def count_campion(champion_to_count, information_champions):
+    for pair in information_champions:
+        champion_to_count[pair[1]] = champion_to_count.get(pair[1], 0) + 1
 
 
 main()
