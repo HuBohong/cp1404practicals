ce0444685375fb38e572bdf751909006bc736c32
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-14 15:28:55 +0800

    Read file wimbledon.csv file and test output

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,3 +4,16 @@ wimbledon game practice
 estimate: 1h30min
 actual:
 """
+
+import csv
+
+FILENAME = "wimbledon.csv"
+
+
+def main():
+    with open(FILENAME,"r", encoding="utf-8-sig") as in_file:
+        reader = csv.reader(in_file)
+        next(reader)
+        print(list(reader))
+
+main()
