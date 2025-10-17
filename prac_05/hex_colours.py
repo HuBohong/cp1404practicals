6410e4594e7ff4bb10cb8bc48ae12417cd894032
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-17 14:29:33 +0800

    Replace user input value to no empty between words

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,16 +1,17 @@
 COLOR_TO_CODE = {"Azure1": "#f0ffff", "Azure2": "#e0eeee", "Azure3": "#c1cdcd", "Azure4": "#838b8b",
                  "Aquamarine1": "#7fffd4", "Aquamarine2": "#76eec6", "Aquamarine4": "#458b74", "Asparagus": "#87a96b",
-                 "Baby Blue": "#89cff0", "Baby Pink": "#f4c2c2"}
+                 "BabyBlue": "#89cff0", "BabyPink": "#f4c2c2"}
 
 
 def main():
-    user_color = input("Enter color code: ").strip().title()
+    """Main function to transfer color to code."""
+    user_color = input("Enter color code: ").strip().title().replace(" ", "")
     while user_color != "":
         try:
-            print(user_color, "is", COLOR_TO_CODE[user_color])
-            user_color = input("Enter color code: ").strip().title()
+            print(user_color, "is", COLOR_TO_CODE[user_color])           # user_color = input("Enter color code: ").strip().title().replace(" ", "")
         except KeyError:
-            user_color = input("Invalid color name please enter correct color code: ").strip().title()
+            print("Invalid color name")           # user_color = input("Invalid color name please enter correct color code: ").strip().title().replace(" ", "")
+        user_color = input("Enter color code: ").strip().title().replace(" ", "")
 
 
 main()
