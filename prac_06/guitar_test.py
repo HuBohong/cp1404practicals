30c65b890ab6a054e6ea8b30071534951ae06fa3
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-28 18:00:08 +0800

    Fix current year in hard code with number of 2025

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,12 +1,12 @@
 from prac_06.guitar import Guitar
-CURRENT_YEAR = 2025
+
 
 def main():
     """Test Guitar class."""
     guitar_1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
     guitar_2 = Guitar("Another Guitar", 2013, 1512.90)
-    print(f"{guitar_1.name} get_age() - Expected {CURRENT_YEAR - guitar_1.year}. Got {guitar_1.get_age(CURRENT_YEAR)}")
-    print(f"{guitar_2.name} get_age() - Expected {CURRENT_YEAR - guitar_2.year}. Got {guitar_2.get_age(CURRENT_YEAR)}")
+    print(f"{guitar_1.name} get_age() - Expected 103. Got {guitar_1.get_age()}")
+    print(f"{guitar_2.name} get_age() - Expected 12. Got {guitar_2.get_age()}")
     print(f"{guitar_1.name} is_vintage() - Expected True. Got {guitar_1.is_vintage()}")
     print(f"{guitar_2.name} is_vintage() - Expected False. Got {guitar_2.is_vintage()}")
 
