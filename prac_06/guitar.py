89e8985df5cbc8d53ff55e960b2232fdf0a0e349
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-28 16:16:47 +0800

    add function calculate age of guitar

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -16,3 +16,7 @@ class Guitar:
     def __str__(self):
         """Return a string representation of a Guitar instance."""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self, current_year):
+        """Return the age of the guitar."""
+        return current_year - self.year
