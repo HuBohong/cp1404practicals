a07d7fe14229071ceadb74fddae76d469dcf8d48
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-28 15:21:34 +0800

    add testing code in clint file

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,7 +1,11 @@
 from prac_06.programming_language import ProgrammingLanguage
 
 def main():
-    pass
+    """Create and display ProgrammingLanguage objects."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
 
 
 main()
