0fe4c18e429dc016dbb99ece8bd484632515c844
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-28 18:31:47 +0800

    update the function print in format and test

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,8 +2,8 @@ from prac_06.guitar import Guitar
 
 
 def main():
-    pass
-
+    guitars = get_guitars()
+    print_in_format(guitars)
 
 def get_guitars():
     guitars = []
@@ -15,7 +15,16 @@ def get_guitars():
         guitars.append(guitar)
         print(f"{guitar} added.")
         guitar_name = input("Name: ")
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
     return guitars
+
+def print_in_format(guitars):
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = "(vintage)" if guitar.is_vintage() else" "
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 
 main()
