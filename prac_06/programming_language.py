46cd9b65fd0708bf2cae662dc5de3b51f1992117
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-28 15:21:02 +0800

    add attribute docstring to each parameter

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,5 +1,12 @@
 class ProgrammingLanguage:
     def __init__(self, name="", typing="", reflection="", year=0):
+        """
+        Initialize a ProgrammingLanguage instance.
+        :param name: name of the programming language
+        :param typing: dynamic or static typing
+        :param reflection: reflection capability (Yes/No)
+        :param year: year of creation
+        """
         self.name = name
         self.typing = typing
         self.reflection = reflection
