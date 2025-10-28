c75f8fc6d7f34a14e76267a439e1911606cbc03b
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-28 16:16:20 +0800

    add print format for guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,8 +1,18 @@
+"""
+Estimate: 30 minutes
+Start time: 4:00
+"""
+
+
 class Guitar:
     """Represent a Guitar object"""
 
-    def __init__(self,name = "", year = 0, cost = 0.0):
+    def __init__(self, name="", year=0, cost=0.0):
         """Initialise a Guitar instance."""
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of a Guitar instance."""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
