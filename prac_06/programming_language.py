c9ba8190287ea5f43adfedf2109badccb7fa14a4
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-28 15:14:35 +0800

    add programming language init function

---
+++ prac_06/programming_language.py
@@ -0,0 +1,8 @@
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection="", year=0):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
