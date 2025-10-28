a04e21dc30f54dec83f494c77103d16229a3296d
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-28 16:56:45 +0800

    update the guitar test

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,6 +1,14 @@
 from prac_06.guitar import Guitar
+CURRENT_YEAR = 2025
 
 def main():
-    pass
+    """Test Guitar class."""
+    guitar_1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    guitar_2 = Guitar("Another Guitar", 2013, 1512.90)
+    print(f"{guitar_1.name} get_age() - Expected {CURRENT_YEAR - guitar_1.year}. Got {guitar_1.get_age(CURRENT_YEAR)}")
+    print(f"{guitar_2.name} get_age() - Expected {CURRENT_YEAR - guitar_2.year}. Got {guitar_2.get_age(CURRENT_YEAR)}")
+    print(f"{guitar_1.name} is_vintage() - Expected True. Got {guitar_1.is_vintage()}")
+    print(f"{guitar_2.name} is_vintage() - Expected False. Got {guitar_2.is_vintage()}")
+
 
 main()
