6118d5cd6c9e366135206b1c5687ba356f4ddc69
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-28 15:55:32 +0800

    add guitar file

---
+++ prac_06/guitar.py
@@ -0,0 +1,8 @@
+class Guitar:
+    """Represent a Guitar object"""
+
+    def __init__(self,name = "", year = 0, cost = 0.0):
+        """Initialise a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
