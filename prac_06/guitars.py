a89a9415b29721522083a2131e7483e8a715fa40
HuBohong <bohong.hu@my.jcu.edu.au>
2025-10-28 18:22:36 +0800

    update the function to get a guitar list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,4 +1,21 @@
+from prac_06.guitar import Guitar
+
+
 def main():
     pass
+
+
+def get_guitars():
+    guitars = []
+    guitar_name = input("Name: ")
+    while guitar_name != "":
+        guitar_year = int(input("Year: "))
+        guitar_cost = float(input("Cost: ").replace("$", ""))
+        guitar = Guitar(guitar_name, guitar_year, guitar_cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        guitar_name = input("Name: ")
+    return guitars
+
 
 main()
