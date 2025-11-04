1040bc930aa6664c430233f4d035884e521c3d7d
HuBohong <bohong.hu@my.jcu.edu.au>
2025-11-04 13:39:13 +0800

    update load and display function in guitar client

---
+++ prac_07/guitar_client.py
@@ -0,0 +1,29 @@
+from prac_07.guitar import Guitar
+
+FILENAME = "guitars.csv"
+
+
+def main():
+    """Create and display Guitar objects."""
+    guitars = load_guitars(FILENAME)
+    display_guitars(guitars)
+
+
+def load_guitars(filename):
+    guitars = []
+
+    with open(filename, "r") as in_file:
+        for line in in_file:
+            name, year, cost = line.strip().split(",")
+            guitar = Guitar(name, int(year), float(cost))
+            guitars.append(guitar)
+
+    return guitars
+
+
+def display_guitars(guitars):
+    for guitar in guitars:
+        print(guitar)
+
+
+main()
