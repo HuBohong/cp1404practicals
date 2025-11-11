0242be13d5c758227076dbb8d6fc9224d5a30492
HuBohong <bohong.hu@my.jcu.edu.au>
2025-11-11 16:26:30 +0800

    Update button up and down event handler

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -5,11 +5,13 @@ CONVERT_TO_KM = 1.60934
 
 class MilesConverterApp(App):
     def build(self):
+        """ build kivy frame """
         self.title = 'Miles Convert to Kilometres'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def convert_miles_km(self):
+        """ convert miles to kilometres"""
         miles = self.get_valid_miles()
         km = miles * CONVERT_TO_KM
         self.root.ids.output_id.text = str(km)
@@ -21,5 +23,11 @@ class MilesConverterApp(App):
             return miles
         except ValueError:
             return 0.0
+
+    def handle_change(self,change):
+        """ calculate change when up or down on click """
+        miles = self.get_valid_miles()
+        new_miles = miles + change
+        self.root.ids.input_miles.text = str(new_miles)
 
 MilesConverterApp().run()
