4916948278de1f17f1314fc9ae2f24e77bc14766
HuBohong <bohong.hu@my.jcu.edu.au>
2025-11-18 18:48:39 +0800

    update customer choice structure

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,8 +1,18 @@
 from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
+MENU = "q)uit, c)hoose taxi, d)rive"
+
 def main():
-    pass
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    current_bill = 0.0
+    customer_choice = input(f"{MENU}\n>>> ").lower()
+    while customer_choice != "q":
+        if customer_choice == "c":
+            pass
+        if customer_choice == "d":
+            pass
 
 
 main()
