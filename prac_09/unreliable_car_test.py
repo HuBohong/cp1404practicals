add0540e8761c65c0f46af348c1d5402092c6c12
HuBohong <bohong.hu@my.jcu.edu.au>
2025-11-18 15:56:02 +0800

    Test for unreliable car within a loop

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,7 +1,11 @@
 from prac_09.unreliable_car import UnreliableCar
 
+TEST_TIME = 10
 
 def main():
-    pass
+    unreliable_car = UnreliableCar("", 500, 0.5)
+    for i in range(TEST_TIME):
+        distance_driven = unreliable_car.drive(20)
+        print(f"{distance_driven} km in {i+1} test drive")
 
 main()
