620da0357098f636d1c32c3b0be0c545f01b62af
HuBohong <bohong.hu@my.jcu.edu.au>
2025-11-18 18:37:48 +0800

    Init commit of taxi simulator

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,8 @@
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+def main():
+    pass
+
+
+main()
--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -3,6 +3,7 @@ from prac_09.unreliable_car import UnreliableCar
 TEST_TIME = 10
 
 def main():
+    """ Test client code to use UnreliableCar class."""
     unreliable_car = UnreliableCar("", 500, 0.5)
     for i in range(TEST_TIME):
         distance_driven = unreliable_car.drive(20)
