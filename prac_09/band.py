e8082d6c46c286eb4359e82d9602ffc06b017408
HuBohong <bohong.hu@my.jcu.edu.au>
2025-11-18 18:04:15 +0800

    Update magic method str to represent the brand by a string

--- prac_09/band.py
+++ prac_09/band.py
@@ -9,6 +9,10 @@ class Band:
         self.name = name
         self.musicians = []
 
+    def __str__(self):
+        """Return a string representation of the band."""
+        return f"{self.name} ({','.join(str(musician) for musician in self.musicians)})"
+
     def add(self, musician: Musician):
         """Add a musician to the band."""
         self.musicians.append(musician)
