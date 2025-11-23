fabb88fcfc5c64a55cf7cfdf3e553811756eada0
HuBohong <bohong.hu@my.jcu.edu.au>
2025-11-23 12:27:06 +0800

    Fix issue with docstring

--- prac_09/my_band.py
+++ prac_09/my_band.py
@@ -5,6 +5,7 @@ from guitar import Guitar
 
 
 def main():
+    """Demo of Band, Musician, and Guitar classes."""
     band = Band("Extreme")
     nuno = Musician("Nuno Bettencourt")
     nuno.add(Guitar("Washburn N4", 1990, 2499.95))
