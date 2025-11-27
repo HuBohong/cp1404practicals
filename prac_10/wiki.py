603bd0963c6e94f97afcb2092383b3de47199bd6
HuBohong <bohong.hu@my.jcu.edu.au>
2025-11-27 13:20:37 +0800

    commit with search townsville and print the tile summary of first paragraph and url

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,18 +4,20 @@ from wikipedia import PageError, DisambiguationError
 
 def main():
     """wiki API practice"""
-    page_title = input("Enter a page title").strip()
+    page_title = input("Enter a page title: ").strip()
     while page_title:
         try:
             page = wikipedia.page(page_title, auto_suggest=False)
-            print(page.content)
+            print(page.title)
+            print(page.summary[:398], end="")
+            print(page.url)
         except DisambiguationError as de:
             print("We need a more specific title. Try one of the following:")
             print(de.options[:5])
 
+        page_title = input("Enter a page title: ").strip()
 
-
-        page_title = input("Enter a page title").strip()
+    print("Thank you.")
 
 
 main()
