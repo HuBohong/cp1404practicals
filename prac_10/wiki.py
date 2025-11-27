644385ec52beaf119088a6af699a3d4aae4dba54
HuBohong <bohong.hu@my.jcu.edu.au>
2025-11-27 13:11:04 +0800

    commit with catch disambiguation error

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,7 +1,21 @@
 import wikipedia
+from wikipedia import PageError, DisambiguationError
+
 
 def main():
-    pass
+    """wiki API practice"""
+    page_title = input("Enter a page title").strip()
+    while page_title:
+        try:
+            page = wikipedia.page(page_title, auto_suggest=False)
+            print(page.content)
+        except DisambiguationError as de:
+            print("We need a more specific title. Try one of the following:")
+            print(de.options[:5])
+
+
+
+        page_title = input("Enter a page title").strip()
 
 
 main()
