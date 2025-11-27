d7a80f128b3735c9597b3a1b813846c90856b013
HuBohong <bohong.hu@my.jcu.edu.au>
2025-11-27 12:20:14 +0800

    Update test

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -69,3 +69,15 @@ doctest.testmod()
 # and one more that you decide is a useful test.
 # Run your doctests and watch the tests fail.
 # Then write the body of the function so that the tests pass.
+def phrase_to_sentence(phrase):
+    """
+    Format a phrase as a sentence: capital letter start, single full stop ending.
+
+    >>> phrase_to_sentence('hello')
+    'Hello.'
+    >>> phrase_to_sentence('It is an ex parrot.')
+    'It is an ex parrot.'
+
+    """
+
+    pass
